6578a053651e2b2afeadf80662e534257d7fccfa
Gia Ho <gia@e9.digital>
2023-06-22 04:12:30 +0000

    Test

--- pages/Page1/jsobjects/VIBObject/VIBObject.js
+++ pages/Page1/jsobjects/VIBObject/VIBObject.js
@@ -4,6 +4,17 @@ export default {
 		const data =  FilePicker1.files[0].data;
 		const [title,...parsed] = csv_parse_sync.parse(data);
 		console.log(parsed.length, parsed[0]);
+		const processed = parsed.map((item) => {
+			const datum = {};
+			
+			title.forEach((t, i) => {
+				datum[t] = item[i];
+			})
+			
+			
+			return datum;
+		})
+		this.data = processed;
 		// return parsed;
 	},
 	onUploadCSV (a,b,c) {
